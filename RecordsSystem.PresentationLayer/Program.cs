e227a796549c4f100f6e720a253ad3fe7a743d00
velly petrus <velly.vcorporation@gmail.com>
2023-07-11 15:13:33 +0200

    Add the Dependency injection in the program.cs file as "Scoped" for life-time connection

--- RecordsSystem.PresentationLayer/Program.cs
+++ RecordsSystem.PresentationLayer/Program.cs
@@ -1,7 +1,35 @@
+using RecodsSystem.BusinessLogicLayer.AddressDetailRepository;
+using RecodsSystem.BusinessLogicLayer.CompanyDetailRepository;
+using RecodsSystem.BusinessLogicLayer.EmployeeDetailRepository;
+using RecodsSystem.BusinessLogicLayer.RecUserDetailRepository;
+using RecordsSystem.DataAccessLayer.Interfaces;
+using RecordsSystem.DataAccessLayer.Interfaces.AddressDetailRepository;
+using RecordsSystem.DataAccessLayer.Interfaces.CompanyDetailRepository;
+using RecordsSystem.DataAccessLayer.Interfaces.EmployeeDetailRepository;
+using RecordsSystem.DataAccessLayer.Interfaces.RecUserDetailRepository;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+//builder.Services.AddDbContext<ApplicationDbContext>(options =>
+//    options.UseSqlServer(builder.Configuration
+//    .GetConnectionString("ApplicationConnectionString")));
+builder.Services.AddScoped<IRecUseDetailRepository, RecUserDetailRepository>();
+builder.Services.AddScoped<IRecUserDetailService, RecUserDetailService>();
+
+builder.Services.AddScoped<IEmployeeDetailRepository, EmployeeDetailRepository>();
+builder.Services.AddScoped<IEmployeeDetailService, EmployeeDetailService>();
+
+
+builder.Services.AddScoped<ICompanyDetailRepository, CompanyDetailRepository>();
+builder.Services.AddScoped<ICompanyDetailService, CompanyDetailService>();
+
+builder.Services.AddScoped<IAddressDetailRepository, AddressDetailRepository>();
+builder.Services.AddScoped<IAddressDetailService, AddressDetailService>();
+
+//builder.Services.AddScoped<>();
+
 
 var app = builder.Build();
 
